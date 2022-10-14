cb298e0fe062658c5b1672c43081cfe357b93f36
Wetooa <103910243+Wetooa@users.noreply.github.com>
2022-10-14 21:09:33 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { FormEvent, useEffect, useRef } from "react";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDf8MUUNBNNXREUdF6Lr-NovwRbvfYJWaY",
+  authDomain: "messenger-clone-5ab6b.firebaseapp.com",
+  projectId: "messenger-clone-5ab6b",
+  storageBucket: "messenger-clone-5ab6b.appspot.com",
+  messagingSenderId: "521203749407",
+  appId: "1:521203749407:web:600f08b0044dab89097b2a",
+  measurementId: "G-7CZX1SXBT0",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth as any);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>THIS IS A STUPID MESSENGER CLONE</header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
+  );
+}
+
+function ChatRoom() {
+  const messageRef = firestore.collection("messages");
+
+  const query = messageRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query as any, { idField: "id" } as any);
+
+  const messageContainer: React.MutableRefObject<any> = useRef();
+  const scrollRef: React.MutableRefObject<any> = useRef();
+
+  const sendMessage = async (e: FormEvent): Promise<void> => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser!;
+
+    await messageRef.add({
+      text: messageContainer.current.value,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    messageContainer.current.value = "";
+    scrollRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    messageContainer.current && messageContainer.current.focus();
+  }, []);
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg, index) => {
+            return <ChatMessage key={index} {...msg} />;
+          })}
+
+        <div ref={scrollRef}></div>
+      </main>
+      <form>
+        <input ref={messageContainer} type="text" id="message" />
+        <button type="submit" onClick={sendMessage}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(message: any) {
+  const { text, uid, photoURL } = message;
+  console.log(photoURL);
+  console.log(message);
+
+  const messageClass = uid === auth.currentUser?.uid ? "sent" : "recieved";
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} alt={text} />
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
   );
 }
 
